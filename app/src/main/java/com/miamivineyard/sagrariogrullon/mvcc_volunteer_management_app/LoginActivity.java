ddf0493f1f0a3fc5b8113123e021e67499f3cd7a
Sagrario Grullon <Sagrario Grullon>
2016-11-03 18:44:16 -0400

    Sagrario 11-3-2016: changing label text for testing

--- app/src/main/java/com/miamivineyard/sagrariogrullon/mvcc_volunteer_management_app/LoginActivity.java
+++ app/src/main/java/com/miamivineyard/sagrariogrullon/mvcc_volunteer_management_app/LoginActivity.java
@@ -46,7 +46,7 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
 
     /**
      * A dummy authentication store containing known user names and passwords.
-     * TODO: remove after connecting to a real authentication system.
+     * TODO: Remove after connecting to a real authentication system.
      */
     private static final String[] CREDENTIALS = new String[]{
             "foo@example.com:hello", "bar@example.com:world"
