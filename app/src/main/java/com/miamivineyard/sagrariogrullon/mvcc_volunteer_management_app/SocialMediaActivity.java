b31b6878bf4d2b727dd3921517ef1eb067b4712e
Sagrario Lind <s-lind@outlook.com>
2016-11-04 17:23:44 -0400

    SG-11-4-16: Adding a few things to test Google Firebase set up.

--- app/src/main/java/com/miamivineyard/sagrariogrullon/mvcc_volunteer_management_app/SocialMediaActivity.java
+++ app/src/main/java/com/miamivineyard/sagrariogrullon/mvcc_volunteer_management_app/SocialMediaActivity.java
@@ -1,7 +1,7 @@
 package com.miamivineyard.sagrariogrullon.mvcc_volunteer_management_app;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class SocialMediaActivity extends AppCompatActivity {
 
