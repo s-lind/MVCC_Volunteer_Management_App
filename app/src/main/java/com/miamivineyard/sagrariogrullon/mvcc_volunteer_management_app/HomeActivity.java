18a12f5fa735d0ef4b896874cd50bf3f96b6d52d
guesag <guesag@gmail.com>
2016-11-17 19:26:53 -0500

    Background change

--- app/src/main/java/com/miamivineyard/sagrariogrullon/mvcc_volunteer_management_app/HomeActivity.java
+++ app/src/main/java/com/miamivineyard/sagrariogrullon/mvcc_volunteer_management_app/HomeActivity.java
@@ -3,6 +3,7 @@ package com.miamivineyard.sagrariogrullon.mvcc_volunteer_management_app;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+
 public class HomeActivity extends AppCompatActivity {
 
     @Override
