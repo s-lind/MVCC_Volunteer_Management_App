67ec0ce2ecd85bb37eef5894186d586eb3fc85ac
Sagrario Lind <s-lind@outlook.com>
2016-11-20 21:24:34 -0500

    SG-11-20-16: Applying quick fix for prayer request xml layout file, i was missing an xml file under the res/drawable folder for the @drawable/backwithborder.xml.

--- app/src/main/java/com/miamivineyard/sagrariogrullon/mvcc_volunteer_management_app/GiveActivity.java
+++ app/src/main/java/com/miamivineyard/sagrariogrullon/mvcc_volunteer_management_app/GiveActivity.java
@@ -2,6 +2,8 @@ package com.miamivineyard.sagrariogrullon.mvcc_volunteer_management_app;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.method.LinkMovementMethod;
+import android.widget.TextView;
 
 public class GiveActivity extends AppCompatActivity {
 
@@ -9,5 +11,12 @@ public class GiveActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_give);
+
+        //link web giving
+        TextView mLink = (TextView) findViewById(R.id.txtvwGiveOnline);
+        if (mLink != null) {
+            mLink.setMovementMethod(LinkMovementMethod.getInstance());
+        }
+
     }
 }
