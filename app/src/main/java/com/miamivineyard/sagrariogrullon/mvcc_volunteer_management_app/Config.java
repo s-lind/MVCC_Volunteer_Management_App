7bfd06ab39077c19a3e331c3fa71ca3770260cae
guesag <guesag@gmail.com>
2016-11-28 16:09:03 -0500

    Email Test

---
+++ app/src/main/java/com/miamivineyard/sagrariogrullon/mvcc_volunteer_management_app/Config.java
@@ -0,0 +1,7 @@
+package com.miamivineyard.sagrariogrullon.mvcc_volunteer_management_app;
+
+
+public class Config {
+    public static final String EMAIL ="miamivineyard@gmail.com";
+    public static final String PASSWORD ="Test123456";
+}
--- app/src/main/java/com/miamivineyard/sagrariogrullon/mvcc_volunteer_management_app/PrayerRequestActivity.java
+++ app/src/main/java/com/miamivineyard/sagrariogrullon/mvcc_volunteer_management_app/PrayerRequestActivity.java
@@ -1,14 +1,61 @@
 package com.miamivineyard.sagrariogrullon.mvcc_volunteer_management_app;
 
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class PrayerRequestActivity extends AppCompatActivity {
+import java.util.Properties;
+
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+
+public class PrayerRequestActivity extends AppCompatActivity implements View.OnClickListener {
+
+
+
+    //Declaring EditText
+    private EditText editTextEmail;
+    private EditText editTextSubject;
+    private EditText editTextMessage;
+
+    //Send button
+    private Button buttonSend;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_prayer_request);
+
+        //Initializing the views
+        editTextEmail = (EditText) findViewById(R.id.editTextEmail);
+        editTextSubject = (EditText) findViewById(R.id.editTextSubject);
+        editTextMessage = (EditText) findViewById(R.id.editTextMessage);
+
+        buttonSend = (Button) findViewById(R.id.buttonSend);
+
+        //Adding click listener
+        buttonSend.setOnClickListener(this);
+    }
+
+
+    private void sendEmail() {
+        //Getting content for email
+        String email = editTextEmail.getText().toString().trim();
+        String subject = editTextSubject.getText().toString().trim();
+        String message = editTextMessage.getText().toString().trim();
+
+        //Creating SendMail object
+        SendMail sm = new SendMail(this, email, subject, message);
+
+        //Executing sendmail to send email
+        sm.execute();
+    }
+
+    @Override
+    public void onClick(View v) {
+        sendEmail();
     }
 
     //email to send prayer requests to ptretbar@hotmail.com
