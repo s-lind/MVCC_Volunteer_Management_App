cc527b2e7f17b2782179f83739741704ef264340
guesag <guesag@gmail.com>
2016-11-28 19:10:48 -0500

    Fixed Social Links

--- app/src/main/java/com/miamivineyard/sagrariogrullon/mvcc_volunteer_management_app/SocialMediaActivity.java
+++ app/src/main/java/com/miamivineyard/sagrariogrullon/mvcc_volunteer_management_app/SocialMediaActivity.java
@@ -1,8 +1,11 @@
 package com.miamivineyard.sagrariogrullon.mvcc_volunteer_management_app;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.text.method.LinkMovementMethod;
+import android.view.View;
 import android.widget.TextView;
 
 public class SocialMediaActivity extends AppCompatActivity {
@@ -15,10 +18,58 @@ public class SocialMediaActivity extends AppCompatActivity {
         //need to link all image buttons and text views to go to their respective social media site - see Give Activity for example
 
         //Facebook
+
+
         TextView mFacebookLink = (TextView) findViewById(R.id.txtvwFacebook);
         if (mFacebookLink != null) {
             mFacebookLink.setMovementMethod(LinkMovementMethod.getInstance());
         }
+
+        TextView mTwitter = (TextView) findViewById(R.id.txtvwTwitter);
+        if (mTwitter != null) {
+            mTwitter.setMovementMethod(LinkMovementMethod.getInstance());
+        }
+
+        TextView mInstagram = (TextView) findViewById(R.id.txtvwInstagram);
+        if (mInstagram != null) {
+            mInstagram.setMovementMethod(LinkMovementMethod.getInstance());
+        }
+
+        TextView mVimeo = (TextView) findViewById(R.id.txtvwVimeo);
+        if (mVimeo != null) {
+            mVimeo.setMovementMethod(LinkMovementMethod.getInstance());
+        }
+
+        TextView mYoutube = (TextView) findViewById(R.id.txtvwYouTube);
+        if (mYoutube != null) {
+            mYoutube.setMovementMethod(LinkMovementMethod.getInstance());
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+        findViewById(R.id.imgbtnYouTube).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String url = "https://www.youtube.com/user/miamivineyard";
+
+                Intent i = new Intent(Intent.ACTION_VIEW);
+                i.setData(Uri.parse(url));
+                startActivity(i);
+            }
+
+
+
+        });
 
     }
 }
