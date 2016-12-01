3bd982e8ea9854ba6417477a9c2a874bc318cc4f
guesag <guesag@gmail.com>
2016-11-30 22:18:48 -0500

    Added URLs to Buttons and missing url links

--- app/src/main/java/com/miamivineyard/sagrariogrullon/mvcc_volunteer_management_app/BulletinBoardActivity.java
+++ app/src/main/java/com/miamivineyard/sagrariogrullon/mvcc_volunteer_management_app/BulletinBoardActivity.java
@@ -2,12 +2,53 @@ package com.miamivineyard.sagrariogrullon.mvcc_volunteer_management_app;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.res.TypedArray;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.Spinner;
 
 public class BulletinBoardActivity extends AppCompatActivity {
+
+    private ImageView image;
+    private String[] feeds;
+    private Spinner spinner;
+    private TypedArray imgs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bulletin_board);
+        feeds = getResources().getStringArray(R.array.feed_list);
+        imgs = getResources().obtainTypedArray(R.array.feed_img_list);
+
+        image = (ImageView) findViewById(R.id.feed_image);
+        spinner = (Spinner) findViewById(R.id.feed_spinner);
+
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_item, feeds);
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(dataAdapter);
+
+        spinner.setOnItemSelectedListener(new OnItemSelectedListener() {
+
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view,
+                                       int position, long id) {
+                image.setImageResource(imgs.getResourceId(
+                        spinner.getSelectedItemPosition(), -1));
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> arg0) {
+
+            }
+        });
     }
-}
+
+    }
+
