ffc63e7432beffdbccbb226b0871c433b06a2bd9
Sagrario Lind <s-lind@outlook.com>
2016-12-06 01:04:31 -0500

    SG-12-5-16: Started work on Schedule UI.

--- app/src/main/java/com/miamivineyard/sagrariogrullon/mvcc_volunteer_management_app/ScheduleActivity.java
+++ app/src/main/java/com/miamivineyard/sagrariogrullon/mvcc_volunteer_management_app/ScheduleActivity.java
@@ -2,6 +2,8 @@ package com.miamivineyard.sagrariogrullon.mvcc_volunteer_management_app;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class ScheduleActivity extends AppCompatActivity {
 
@@ -9,5 +11,16 @@ public class ScheduleActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_schedule);
+
+        Spinner spinner = (Spinner) findViewById(R.id.spnSelectTeamSchedule);
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.schedule_array, android.R.layout.simple_spinner_item);
+// Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+// Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
+
     }
+
 }
